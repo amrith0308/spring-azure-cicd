d0305133a204239f1d7df59a60c2b17e12478254
amrith0308 <amrith87@gmail.com>
2025-02-20 09:30:10 +0530

    first commit

---
+++ src/main/java/com/amrith/spring_sample_app/HomeController.java
@@ -0,0 +1,13 @@
+package com.amrith.spring_sample_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String getIndex() {
+        return "Hello from Spring boot app";
+    }
+}
